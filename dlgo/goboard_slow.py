22313405c832a668e5a65a240ad910011d41ad16
Pawel Wocjan <wocjan@cs.ucf.edu>
2019-02-17 19:31:45 -0500

    Add GameState class

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -136,3 +136,105 @@ class Board:
                 if neighbor_string is not string:
                     neighbor_string.add_liberty(point)
             self._grid[point] = None
+
+
+class GameState:
+    def __init__(self, board, next_player, previous, move):
+        self.board = board
+        self.next_player = next_player
+        self.previous_state = previous
+        self.last_move = move
+
+    def apply_move(self, move):
+        # Returns the new GameState after applying the move
+        if move.is_play:
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player, move.point)
+        else:
+            next_board = self.board
+        return GameState(next_board, self.next_player.other, self, move)
+
+    @classmethod
+    def new_game(cls, board_size):
+        # PW: changed "if" to "assert" to avoid warning
+        # about potentially uninitialized board in return statement
+        assert isinstance(board_size, int)
+        board_size = (board_size, board_size)
+        board = Board(*board_size)
+        return GameState(board, Player.black, None, None)
+
+    def is_over(self):
+        if self.last_move is None:
+            return False
+        if self.last_move.is_resign:
+            return True
+        second_last_move = self.previous_state.last_move
+        if second_last_move is None:
+            return False
+        return self.last_move.is_pass and second_last_move.is_pass
+
+    # To decide if a move is legal, you need to check three rules:
+    # 1. Confirm that the point you want to play is empty.
+    # 2. Check that the move isn't a self-capture.
+    # 3. Confirm that the move doesn't violate the ko rule.
+
+    # ......    The three black stones * have one liberty.
+    # ..O...    Black cannot place * in the one liberty left
+    # .O*O..    because this would result in self-capture.
+    # .O*O.O
+    # .O*.O.
+    #    ^
+    #    liberty
+
+    # ...*..    In this situation, black can place * because
+    # ..O*..    this is not suicide
+    # .O*O*.    this results in the capture of two O by black and
+    # .O*O*O    thereby in two liberties for black.
+    # .O*.O.
+    #    ^
+    #    liberty
+
+    def is_move_self_capture(self, player, move):
+        if not move.is_play:
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        new_string = next_board.get_go_string(move.point)
+        return new_string.num_liberties == 0
+
+    @property
+    def situation(self):
+        return self.next_player, self.board
+
+    def does_move_violate_ko(self, player, move):
+        if not move.is_play:
+            return False
+        # The implementation below is simple and correct,
+        # but it's relatively slow. For each move, you
+        # create a deep copy of the board state and have to compare
+        # this state against all previous states, which adds up over time.
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        next_situation = (player.other, next_board)
+        past_state = self.previous_state
+        while past_state is not None:
+            if past_state.situation == next_situation:
+                return True
+            past_state = past_state.previous_state
+        return False
+
+    def is_valid_move(self, move):
+        if self.is_over():
+            return False
+        if move.is_pass or move.is_resign:
+            return True
+        return (
+            self.board.get(move.point) is None and
+            not self.is_move_self_capture(self.next_player, move) and
+            not self.does_move_violate_ko(self.next_player, move))
+
+
+
+
+
+
